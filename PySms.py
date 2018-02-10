3ec2e42bc8f053ea68b68e34270509096e76378d
Unknown <felixcolon89@gmail.com>
2018-02-10 19:42:25 -0400

    Auto stash before merge of "master" and "origin/master"

--- PySms.py
+++ PySms.py
@@ -1,5 +1,7 @@
 import smtplib
 
+from collections import OrderDict
+
 # Use sms gateway provided by mobile carrier:
 # at&t:     number@mms.att.net
 # t-mobile: number@tmomail.net
@@ -8,8 +10,25 @@ import smtplib
 # claro:    number@vtexto.com
 # boost:    number@sms.myboostmobile.com
 
+print "---PySms---"
 
-message = str(raw_input("Please enter message: "))
+print "Choose carrier\n"
+
+
+gatewayServers = OrderedDict([('AT&T','@mms.att.net'),
+							  ('T-Mobile','@tmomail.net'),
+							  ('Verizon','@vtext.com'),
+							  ('Sprint','@page.nextel.com'),
+							  ('Claro','@vtexto.com'),
+							  ('Boost','@sms.myboostmobile.com')])
+
+for i,x in enumerate(gatewayServers, start=1):
+    print str([i]),x
+
+gateway = raw_input("\nChoose gateway: ")
+message = raw_input("Please enter message: ")
+number = raw_input("Enter target phone number: ")
+
 # Establish a secure session with gmail's outgoing SMTP server using your gmail account
 server = smtplib.SMTP( "smtp.gmail.com", 587)
 
@@ -24,4 +43,3 @@ server.login( '<email>', '<password>' )
 
 # follow comments above for choosing  target number
 server.sendmail( 'foo@bar.com', '<target number>', message)
- 
